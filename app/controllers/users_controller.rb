584c69fa10969f952e2652a1731b909a2b20d28b
Keigo Ogawa <k5.ba.br@gmail.com>
2014-09-14 06:37:24 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,86 @@
 class UsersController < ApplicationController
+ #before_action :non_signed_in_user, only: [:new, :create]
+  before_action :signed_in_user,     only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,       only: [:edit, :update]
+  before_action :admin_user,         only: :destroy
+
+    def index
+      @users = User.paginate(page: params[:page])
+    end
+
   	def show
   	@user = User.find(params[:id])
   	end
 
   	def new
-  		@user = User.new
+      if signed_in?
+        redirect_to root_path
+      else
+  		  @user = User.new
+      end
   	end
 
   	def create
-  		@user = User.new(user_params)
-  		if @user.save
-        sign_in @user
-  			flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user
-  		else
-  			render 'new'
-  		end
+      if signed_in?
+        redirect_to root_path
+      else
+    		@user = User.new(user_params)
+    		if @user.save
+          sign_in @user
+    			flash[:success] = "Welcome to the Sample App!"
+    			redirect_to @user
+    		else
+    			render 'new'
+    		end
+      end
   	end
+
+    def edit
+    end
+
+    def update
+      if @user.update_attributes(user_params)
+        #if success update
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      if current_user? user
+        redirect_to(root_path)
+      else
+        User.find(params[:id]).destroy
+        flash[:success] = "User destroyed."
+        redirect_to users_url
+      end
+    end
 
   	private
   		def user_params
   			params.require(:user).permit(:name, :email, :password, :password_confirmation)
   		end
+
+      #Before actions
+
+      #def non_signed_in_user
+       # redirect_to(root_path)        
+      #end
+
+      def signed_in_user
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)        
+      end
+
+      def admin_user
+        redirect_to(root_path) unless current_user.admin?          
+      end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,33 +11,153 @@ describe "Authentication" do
 	end
 
 	describe "signin" do
-		before { visit signin_path }
+			before { visit signin_path }
 
-		describe "with invalid information" do
-			before { click_button "Sign in" }
+	
+			describe "with invalid information" do
+                let(:user) { FactoryGirl.create(:user) }
+				before { click_button "Sign in" }
+	
+				it { should have_title('Sign in') }
+				it { should have_error_message('Invalid') }
+                it { should_not have_link('Profile',        href: user_path(user) )}
+                it { should_not have_link('Settings',       href: edit_user_path(user))}
+	
+				describe "after visiting another page" do
+					before { click_link "Home" }
+					it { should_not have_selector('div.alert.alert-error') }
+				end
+			end
+	
+			describe "with valid information" do
+				let(:user) { FactoryGirl.create(:user) }
+				before { sign_in user }
+	
+				it { should have_title(user.name) }
+                it { should have_link('Users',          href: users_path)}
+				it { should have_link('Profile', 		href: user_path(user) )}
+				it { should have_link('Settings', 		href: edit_user_path(user))}
+				it { should have_link('Sign out', 		href: signout_path) }
+				it { should_not have_link('Sign in', 	href: signin_path) }
+	
+				describe "followed by sign out" do
+					before { click_link "Sign out" }
+					it { should have_link('Sign in') }
+				end
+			end
+	end
+	
+	describe "authorization" do
+	
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user)}
 
-			it { should have_title('Sign in') }
-			it { should have_error_message('Invalid') }
+            describe "when attempting to visit a protected page" do
+                before do
+                    visit edit_user_path(user)
+                    fill_in "Email",    with: user.email
+                    fill_in "Password", with: user.password 
+                    click_button "Sign in"
+                end
 
-			describe "after visiting another page" do
-				before { click_link "Home" }
-				it { should_not have_selector('div.alert.alert-error') }
+                describe "after signing in" do
+                    it "should render the desired protected page" do
+                        expect(page).to have_title('Edit user')
+                    end
+
+                    describe "when signing in again" do
+                        before do
+                            click_link "Sign out"
+                            visit signin_path
+                            fill_in "Email",    with: user.email
+                            fill_in "Password", with: user.password
+                            click_button "Sign in"
+                        end
+
+                        it  "should render the default page" do
+                            expect(page).to have_title(user.name)
+                        end
+                    end
+                end
+            end
+
+			describe "in the Users controller" do
+	
+				describe "visiting the edit page" do
+					before {visit edit_user_path(user)}
+					it {should have_title('Sign in')}
+				end
+
+				describe "submitting to the update action" do
+					before { patch user_path(user)}
+					specify { expect(response).to redirect_to(signin_path)}
+				end
+
+                describe "visiting the user index" do
+                    before {visit users_path} 
+                    it { should have_title('Sign in')}
+                end
 			end
 		end
 
-		describe "with valid information" do
-			let(:user) { FactoryGirl.create(:user) }
-			before { valid_signin(user) }
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user)}
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com")}
+			before { sign_in user, no_capybara: true}
 
-			it { should have_title(user.name) }
-			it { should have_link('Profile', href: user_path(user) )}
-			it { should have_link('Sign out', href: signout_path) }
-			it { should_not have_link('Sign in', href:signin_path) }
+			describe "submitting a GET request to the Users#edit action" do
+				before { get edit_user_path(wrong_user)}
+				specify { expect(response.body).not_to match(full_title('Edit user'))}
+				specify { expect(response).to redirect_to(root_url)}
+			end
 
-			describe "followed by sign out" do
-				before { click_link "Sign out" }
-				it { should have_link('Sign in') }
+			describe "submitting a PATCH request to the Users#update action" do
+				before  { patch user_path(wrong_user)}
+				specify { expect(response).to redirect_to(root_path)}
 			end
 		end
+
+        describe "as non-admin user" do
+            let(:user) {FactoryGirl.create(:user)}
+            let(:non_admin) { FactoryGirl.create(:user)}
+
+            before { sign_in non_admin, no_capybara: true}
+
+            describe "submitting a DELETE request to the Users#destroy action" do
+                before { delete user_path(user)}
+                specify { expect(response).to redirect_to(root_path) }
+            end
+        end
+
+        describe "for signed-in users" do
+            let(:user) { FactoryGirl.create(:user)}
+            before { sign_in user, no_capybara: true}
+
+            describe "should redirect to root if user tries to hit new" do
+                before { get new_user_path}
+                specify { expect(response).to redirect_to(root_url)}
+            end
+
+            describe "should redirect to root if user tries to hit create" do
+                let(:params) do
+                { user: { name:"test", email: "test@test.com", password: "foobar",
+                        password_confirmation: "foobar" } }
+                    end
+                before {post users_path, params}
+                specify { expect(user.reload).not_to be_admin}
+            end
+        end 
+
+        describe "as an admin user" do
+            let(:admin) { FactoryGirl.create(:admin)}
+            before do
+                sign_in admin, no_capybara:true
+            end
+
+            describe "submitting a DELETE request to the Users#destroy action" do
+                before { delete user_path(admin)}
+                specify { expect(response).to redirect_to(root_path)}
+            end
+        end      
 	end
 end
